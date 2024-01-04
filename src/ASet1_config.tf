b082673846194beecc889adb6f0d7ad70dd45ab4
rjxwat <143182963+rjxwat@users.noreply.github.com>
2024-01-04 18:54:59 +0530

    ASet1_config.tf

--- src/ASet1_config.tf
+++ src/ASet1_config.tf
@@ -7,16 +7,6 @@ terraform {
   }
 }
 
-provider "azurerm" {
-    subscription_id = "e6c53357-7186-450c-a856-36e02ed9bee5"
-    client_id = "894961f1-0942-4322-a7e5-1ff90da005f9"
-    client_secret = "BY.8Q~xxpgMCHh8He~UfPrBqoClZZN52V0rTNaf-"
-    tenant_id = "89095e62-86bd-45b3-9414-364373853d58"
-  
-  features {
-    
-  }
-}
 
 
 locals {
